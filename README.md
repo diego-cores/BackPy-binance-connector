7f414e368364bde5bffee1c1e0141ccd805b7f72
Diego Cores <diegocores.047@gmail.com>
2024-11-25 20:44:45 -0300

    feat: improved documentation and init file
    - Updated __init__ with documentation and better syntax.
    - Risk_notice.txt changed.
    - Added new README.md documentation and images.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
+![BackPy-binance-connector logo](images/logo.png)
 # BackPy-binance-connector
-This module connects your BackPy strategy with the real market using binance.
+BackPy-binance-connector is a library for creating strategies and automating them in real life using the Binance API.
+
+  #### Version: 0.0.0.001
+
+It is a library that seeks to expand the functionalities of BackPy by 
+providing the possibility of carrying out real trading using the Binance API.
+
+## Important motices:
+
+Please make sure to read the following before using this software:
+
+- [Risk Notice](Risk_notice.txt)
+- [License](LICENSE)
+
+By using this software, you acknowledge that you have read and agree to the terms outlined in these documents.
+
+## How to install backpy with pip:
+
+1. Download the latest version from GitHub
+- Go to this project GitHub page.
+- Look for the "Releases" or "Latest release" section.
+- Download the ZIP file of the latest version of the project.
+2. Unzip the ZIP file
+- Unzip the ZIP file you downloaded.
+- This will give you a folder containing the project files.
+3. Open the terminal
+- Open the terminal in your operating system.
+- Navigate to the folder you just unzipped. You can use the cd command to change directories.
+4. Install the module
+- Once you are in the project folder in terminal, run the following command: 'pip install .'.
+- This command will install the Python module using the setup.py file located in the project folder.
+5. Verify installation
+- After the installation process finishes without errors, you can verify if the module has been installed correctly by running some code that imports the newly installed module.
+6. Clean downloaded files
+- After you have verified that the module is working correctly, you can delete the downloaded ZIP file and unzipped folder if you wish.
--- backpyf_connector/__init__.py
+++ backpyf_connector/__init__.py
@@ -1,5 +1,63 @@
+"""
+Back Test Py Binance Connector.
+
+BackPy-binance-connector is a library for creating strategies and automating them in real life using the Binance API.
+
+Version:
+    0.0.0.001
+
+Repository:
+    https://github.com/Diego-Cores/BackPy-binance-connector
+
+License:
+    MIT License
+
+    Copyright (c) 2024 Diego
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
 
 from . import main
 from . import tradetools
 from . import strategy
 from . import utils
+
+__doc__ = """
+BackPy-binance-connector documentation.
+
+BackPy-binance-connector is a library that seeks to expand the functionalities of BackPy by 
+providing the possibility of carrying out real trading using the Binance API.
+
+Important Notice:
+    Understanding the Risks of Trading and Financial Data Analysis.
+
+    Trading financial instruments and using financial data for analysis 
+    involves significant risks, including the possibility of loss of 
+    capital. Markets can be volatile and data may contain errors. Before 
+    engaging in trading activities or using financial data, it is important 
+    to understand and carefully consider these risks and seek independent 
+    financial advice if necessary.
+"""
+
+__all__ = [
+    'main',
+    'tradetools',
+    'strategy',
+    'utils',
+]
